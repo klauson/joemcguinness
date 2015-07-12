873fde9a9ad27d3a5fa90c73cb7372717778fa0e
klauson <zacc.ledford@gmail.com>
2015-07-11 23:11:42 -0400

    tour and contact

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -53,8 +53,150 @@ $(function(){
   	// Starts fitvids
     $(".mid").fitVids();
 
-    fitText(document.getElementById('fittext'), 1.8, { minFontSize: '22px', maxFontSize: '80px' });
-    fitText(document.getElementById('fittext2'), 1.8, { minFontSize: '22px', maxFontSize: '80px' });
+    fitText(document.getElementById('fittext'), 1.9, { minFontSize: '22px', maxFontSize: '70px' });
+    fitText(document.getElementById('fittext2'), 1.7, { minFontSize: '22px', maxFontSize: '90px' });
+
+        $("#amazingaudioplayer-1").amazingaudioplayer({
+
+        jsfolder:jsFolder,
+
+        skinsfoldername:"",
+
+        titleinbarwidthmode:"auto",
+
+        timeformatlive:"%CURRENT% / LIVE",
+
+        volumeimagewidth:28,
+
+        barbackgroundimage:"",
+
+        showtime:true,
+
+        titleinbarwidth:80,
+
+        showprogress:true,
+
+        random:false,
+
+        titleformat:"%TITLE%",
+
+        height:600,
+
+        loadingformat:"Loading...",
+
+        prevnextimage:"prevnext-24-24-0.png",
+
+        showinfo:true,
+
+        imageheight:150,
+
+        skin:"Jukebox",
+
+        loopimage:"loop-24-24-0.png",
+
+        loopimagewidth:24,
+
+        showstop:false,
+
+        prevnextimageheight:24,
+
+        infoformat:"%ARTIST% %ALBUM%<br />%INFO%",
+
+        stopotherplayers:true,
+
+        showloading:false,
+
+        forcefirefoxflash:false,
+
+        showvolumebar:true,
+
+        imagefullwidth:false,
+
+        width:350,
+
+        showtitleinbar:false,
+
+        showloop:false,
+
+        volumeimage:"volume-24-24-0.png",
+
+        playpauseimagewidth:30,
+
+        loopimageheight:24,
+
+        tracklistitem:10,
+
+        tracklistitemformat:"%ID%. %TITLE% <span style='position:absolute;top:0;right:0;'>%DURATION%</span>",
+
+        prevnextimagewidth:30,
+
+        tracklistarrowimage:"tracklistarrow-48-16-0.png",
+
+        playpauseimageheight:24,
+
+        showbackgroundimage:false,
+
+        imagewidth:150,
+
+        stopimage:"stop-24-24-0.png",
+
+        playpauseimage:"playpause-24-24-0.png",
+
+        showprevnext:true,
+
+        backgroundimage:"",
+
+        autoplay:false,
+
+        volumebarpadding:8,
+
+        progressheight:20,
+
+        showtracklistbackgroundimage:false,
+
+        titleinbarscroll:true,
+
+        showtitle:true,
+
+        defaultvolume:-1,
+
+        tracklistarrowimageheight:16,
+
+        heightmode:"auto",
+
+        titleinbarformat:"%TITLE%",
+
+        showtracklist:true,
+
+        stopimageheight:24,
+
+        volumeimageheight:24,
+
+        stopimagewidth:24,
+
+        volumebarheight:80,
+
+        noncontinous:false,
+
+        tracklistbackgroundimage:"",
+
+        showbarbackgroundimage:false,
+
+        showimage:true,
+
+        tracklistarrowimagewidth:48,
+
+        timeformat:"%CURRENT% / %DURATION%",
+
+        showvolume:true,
+
+        fullwidth:false,
+
+        loop:0,
+
+        preloadaudio:true
+
+    });
   }
 
 
